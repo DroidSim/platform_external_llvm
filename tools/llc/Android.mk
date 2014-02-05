b24e7d67f296a2d148c454bbf1b9d0253a355f94
Colin Cross <ccross@android.com>
2014-02-04 16:26:26 -0800

    llvm: convert makefiles to support multilib build
    Convert makefiles to allow for building two architectures at the
    same time.  This will also cause make checkbuild to build the target
    libraries for all supported architectures.
    
    Change-Id: Ia5e6fe5b1186a67753faafd3532ed4cb280a8b10

--- tools/llc/Android.mk
+++ tools/llc/Android.mk
@@ -91,9 +91,14 @@ include $(BUILD_HOST_EXECUTABLE)
 # llc command line tool (target)
 #===---------------------------------------------------------------===
 
-#TODOArm64: Enable llc build
-#TODOMips64: Enable llc build
-ifeq ($(filter $(TARGET_ARCH),arm64 mips64),)
+ifeq ($(TARGET_ARCH),arm64)
+$(warning TODOArm64: Enable llc build)
+endif
+
+ifeq ($(TARGET_ARCH),mips64)
+$(warning TODOMips64: Enable llc build)
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := llc
@@ -103,18 +108,13 @@ LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES := $(llvm_llc_SRC_FILES)
 LOCAL_C_INCLUDES += external/llvm/include
 
-ifeq ($(TARGET_ARCH),arm)
-  LOCAL_STATIC_LIBRARIES := $(llvm_llc_arm_STATIC_LIBRARIES)
-else
-  ifeq ($(TARGET_ARCH),mips)
-    LOCAL_STATIC_LIBRARIES := $(llvm_llc_mips_STATIC_LIBRARIES)
-  else
-    ifeq ($(TARGET_ARCH),$(filter $(TARGET_ARCH),x86 x86_64 x32))
-      LOCAL_STATIC_LIBRARIES := $(llvm_llc_x86_STATIC_LIBRARIES)
-    else
-      $(error "Unsupport llc target $(TARGET_ARCH)")
-    endif
-  endif
+LOCAL_STATIC_LIBRARIES_arm := $(llvm_llc_arm_STATIC_LIBRARIES)
+LOCAL_STATIC_LIBRARIES_mips := $(llvm_llc_mips_STATIC_LIBRARIES)
+LOCAL_STATIC_LIBRARIES_x86 := $(llvm_llc_x86_STATIC_LIBRARIES)
+LOCAL_STATIC_LIBRARIES_x86_64 := $(llvm_llc_x86_STATIC_LIBRARIES)
+
+ifeq (,$(filter $(TARGET_ARCH),arm arm64 mips x86 x86_64))
+  $(error Unsupported architecture $(TARGET_ARCH))
 endif
 
 LOCAL_STATIC_LIBRARIES += $(llvm_llc_STATIC_LIBRARIES)
@@ -128,5 +128,3 @@ include $(LLVM_ROOT_PATH)/llvm.mk
 include $(LLVM_DEVICE_BUILD_MK)
 include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_EXECUTABLE)
-
-endif # !(arm64 || mips64)
